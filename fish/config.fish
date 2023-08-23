9abbad98ccc2ce1cf24dbfe5620232c0aadc45ca
moon <114552693+blvvmxxn@users.noreply.github.com>
2023-08-23 21:35:49 +0530

    update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -2,11 +2,11 @@ if status is-interactive
     
     set fish_greeting
 
-    alias pman='pacman'
     alias cl='clear'
     alias vim='nvim'
     alias ls='exa --tree --level=1 --sort=type'
     alias la='exa --tree --sort=type'
     alias pl='mpv'
-    alias yta='yt-dlp --extract-audio --audio-format aac'
+    alias nfetch='fastfetch'
+    alias yta='yt-dlp --extract-audio --audio-format opus'
 end
