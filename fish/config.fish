4f8a61f1c6d13fda7f459c7b84f1985f4adfc4d0
moon <bmoon4971@gmail.com>
2023-09-19 10:32:35 +0530

    added fish.config

---
+++ fish/config.fish
@@ -0,0 +1,13 @@
+if status is-interactive
+    
+    set fish_greeting
+
+    alias vim='nvim'
+    alias cl='clear'
+    alias ls='eza --tree --level=1 --sort=type'
+    alias la='eza --tree --sort=type'
+    alias pl='mpv'
+    alias py='python'
+    alias nfetch='fastfetch'
+    alias yt='yt-dlp --extract-audio --audio-format opus'
+end
